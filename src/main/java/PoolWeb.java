c0ad3dc0e266a05407ec5ec658cdb424c9c2ae68
eugegiuliodori <eugegiuliodori@gmail.com>
2020-04-03 14:38:23 -0300

    manejo de error de json que devuelve servidor

--- src/main/java/PoolWeb.java
+++ src/main/java/PoolWeb.java
@@ -10,22 +10,43 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class PoolWeb {
 	
 	private String json;
-	private static int DNI_INICIO;
-	private static int DNI_FIN;
+	private static Integer DNI_INICIO;
+	private static Integer DNI_FIN;
+	private Integer dni;
 
 
 	
 	public PoolWeb() {
 		DNI_INICIO = 1;
-		DNI_FIN = 10;
+		DNI_FIN = 1000;
+	}
+	
+	private boolean isJSONValid(String test) {
+	    try {
+	        new JSONObject(test);
+	    } 
+	    catch (JSONException ex) {
+	        try {
+	            new JSONArray(test);
+	        } 
+	        catch (JSONException ex1) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
 
-	private void generarPeticion(Integer dni) {
+	private String obtenerClienteJSON(Integer dni) {
 		URL url;
+		String currentJSON=new String("");
 		try {
 			url = new URL("http://centro-estetica.ddns.net/api/v1/pacientes/activo?dni="+dni);
 			URLConnection con = url.openConnection();
@@ -37,26 +58,35 @@ public class PoolWeb {
 			}
 			String atr_value_dni="\"dni\":\""+dni.toString()+"\"";
 			String atr_value_apenom=str.split(",")[1].replaceAll("}", "");
-			String currentStr = "{"+atr_value_dni+","+atr_value_apenom+"}";
-			System.out.println(currentStr);
-			json=json+currentStr+",";		
+			currentJSON = "{"+atr_value_dni+","+atr_value_apenom+"}";
+			System.out.println(currentJSON);	
 		} 
 		catch (IOException e) {
 			System.out.println("No existe DNI "+dni);
 		}
+		return currentJSON;
 	}
 
 	public String obtenerClientesJSON() {
 		List<Future> futureTaskList = new LinkedList<>();
 		ExecutorService executor = Executors.newFixedThreadPool(100);
 
-		for (int dni = DNI_INICIO; dni < DNI_FIN; dni++) {
+		for (dni = DNI_INICIO; dni.compareTo(DNI_FIN)<=0; dni++) {
 			int dniCliente = dni;
 			json=new String("");
 			Runnable thread = new Runnable() {
 				@Override
 				public void run() {
-					generarPeticion(dniCliente);
+					String currentJSON = obtenerClienteJSON(dniCliente);
+					if(isJSONValid(currentJSON)) {
+						json=json+currentJSON+",";
+					}
+					else {
+						if(!currentJSON.contains("\"}")&&(currentJSON.contains("}"))) {
+							currentJSON=currentJSON.replaceAll("}", "\"}");
+							json=json+currentJSON+",";
+						}
+					}
 				}
 			};
 			FutureTask<String> task = new FutureTask(thread, "thread done");
